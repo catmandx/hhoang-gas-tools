aad6db2f5217ed1a7997034e7a9ea1fe84ffc100
Hoang Nguyen <50922013+catmandx@users.noreply.github.com>
2020-03-16 21:23:11 +0700

    Added more sheets

--- high5-permission-sync/mainFuncs.js
+++ high5-permission-sync/mainFuncs.js
@@ -10,41 +10,45 @@ function checkActiveMembers(e) { //OnEdit trigger //periodic trigger
   //get active members spreadsheet and sheet
   var ss = SpreadsheetApp.openByUrl("https://docs.google.com/spreadsheets/d/11IcDpPKb-CXlN724c9J5PbickjgoSIQkFJwlgi2-LIA/edit");
   var sheet = ss.getSheetByName("Active Members");
+  var syncSheets = ["https://docs.google.com/spreadsheets/d/11IcDpPKb-CXlN724c9J5PbickjgoSIQkFJwlgi2-LIA/edit",
+  "https://docs.google.com/spreadsheets/d/1-ZGqLy2Qn4jtPt8TJYaEArK4TwYyRkRYxYgDu4C_eyA/edit"];
+  
+  for (const sheetLink of syncSheets) {
+    var orderSheet = SpreadsheetApp.openByUrl(sheetLink);
 
+    var activeMembersList = ["high5hanoi@gmail.com", "cm.high5hanoi@gmail.com", "hrhigh5hanoi@gmail.com", "mdhigh5material@gmail.com",
+                    "edhigh5hanoi@gmail.com", "high5hanoi.edu.vn@gmail.com", "hhoang.nov.13@gmail.com"];
+    var ordersSheetEditorsList = [];
 
-  var activeMembersList = ["high5hanoi@gmail.com", "cm.high5hanoi@gmail.com", "hrhigh5hanoi@gmail.com", "mdhigh5material@gmail.com",
-                  "edhigh5hanoi@gmail.com", "high5hanoi.edu.vn@gmail.com", "hhoang.nov.13@gmail.com"];
-  var ordersSheetEditorsList = [];
-
+    orderSheet.getEditors().forEach((user) => ordersSheetEditorsList.push(user.getEmail()));
+    
+    sheet.getRange("activeMembersList").getValues().forEach((row) => {
+      if(checkEmail(row[0].trim())){
+        activeMembersList.push(row[0]);
+      }
+    });
+    
+    var editorsToAdd = activeMembersList.filter(el => ordersSheetEditorsList.indexOf(el) < 0);
+    
+    var editorsToRemove = ordersSheetEditorsList.filter(email => activeMembersList.indexOf(email) < 0);
+    
+    console.log("Start adding");
+    editorsToAdd.forEach((email) => {
+      console.log(email)
+      try{
+        orderSheet.addEditor(email)
+      }catch(ex){console.log(ex)}
+    });
+    
+    console.log("Start removing");
+    editorsToRemove.forEach((email) => {
+      console.log(email)
+      try{
+        orderSheet.removeEditor(email)
+      }catch(ex){console.log(ex)}
+    });
+  }
   
-  var orderSheet = SpreadsheetApp.openByUrl("https://docs.google.com/spreadsheets/d/1-ZGqLy2Qn4jtPt8TJYaEArK4TwYyRkRYxYgDu4C_eyA/edit");
-  
-  orderSheet.getEditors().forEach((user) => ordersSheetEditorsList.push(user.getEmail()));
-  
-  sheet.getRange("activeMembersList").getValues().forEach((row) => {
-    if(checkEmail(row[0].trim())){
-      activeMembersList.push(row[0]);
-    }
-  });
-  
-  var editorsToAdd = activeMembersList.filter(el => ordersSheetEditorsList.indexOf(el) < 0);
-  
-  var editorsToRemove = ordersSheetEditorsList.filter(email => activeMembersList.indexOf(email) < 0);
-  
-  console.log("Start adding");
-  editorsToAdd.forEach((email) => {
-    console.log(email)
-    try{
-      orderSheet.addEditor(email)
-    }catch(ex){console.log(ex)}
-  });
-  console.log("Start removing");
-  editorsToRemove.forEach((email) => {
-    console.log(email)
-    try{
-      orderSheet.removeEditor(email)
-    }catch(ex){console.log(ex)}
-  });
 }
 
 function checkEmail(email) {
