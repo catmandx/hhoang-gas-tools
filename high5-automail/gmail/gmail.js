ffced270f7f05c91e9b342d4a99d4d07ffcaf619
Hoang Nguyen <50922013+catmandx@users.noreply.github.com>
2020-08-02 23:57:46 +0700

    added automail tool

---
+++ high5-automail/gmail/gmail.js
@@ -0,0 +1,102 @@
+//////////////////////////////////////////
+/**
+ * List of functions in this file:
+ * generateOptionTags()
+ * getDraftsInfo()
+ * generateAliasesOptionTags()
+ * getDraftContentById(draftId)
+ */
+//////////////////////////////////////////
+
+/**
+ * Generate <option> tags to be used in the
+ * form. Invoked back-end.
+ * 
+ * <option> tags include gmail drafts id and name.
+ * 
+ * return optionTags
+ * <option value="r-xxxxxx"> Template names </option>
+ * <option value="r-xxxxxx"> Template names </option>
+ * @returns optionTags
+ */
+function generateDraftsOptionTags() {
+    var drafts = getDraftsInfo();
+    var optionTags = "";
+    drafts.forEach((draft) => {
+        if (draft.subject.trim().length == 0) {
+            draft.subject = "(no subject)";
+        }
+        optionTags +=
+            `\n<option value="${draft.id}">
+                ${draft.isStarred ? "★" : ""} 
+                ${draft.subject} 
+                - ${draft.size}
+            </option>`
+    });
+
+    if (optionTags.length == 0) {
+        optionTags = "<option>No draft available!</option>";
+    }
+
+    return optionTags;
+}
+
+/**
+ * Get drafts from Gmail
+ * @returns arr
+ * 
+ * arr = [dr1, dr2, etc];
+ * 
+ * dr1 = {
+ *  id: "r-xxxxx",
+ *  subject: "Subject",
+ *  isStarred: true|false,
+ *  size: "xxKB"
+ * }
+ */
+function getDraftsInfo() {
+    return GmailApp.getDrafts().map(draft => {
+        return {
+            id: draft.getId(),
+            subject: draft.getMessage().getSubject(),
+            isStarred: draft.getMessage().isStarred(),
+            size: (~~(Utilities
+                .newBlob(draft.getMessage().getRawContent())
+                .getBytes().length / 1024)) + "KB"
+        };
+    })
+}
+
+/**
+ * Generate <option> tags to be used in the alias
+ * field of the form
+ * 
+ * <option value="hhoang@high5hanoi.edu.vn">hhoang@abc....</option>
+ * <option value="hhoang@high5hanoi.edu.vn">hhoang@abc....</option>
+ */
+function generateAliasesOptionTags() {
+    var defaultAlias =
+        `<option value='null'>Not selected</option>
+        <option disabled='disabled'>-----</option>`;
+    var aliases = GmailApp.getAliases();
+    if (aliases.length == 0) {
+        return "<option value='null'>No alias</option>";
+    }
+
+    return defaultAlias + aliases.map(alias => {
+        return `<option value="${alias}">${alias}</option>`;
+    });
+}
+
+/**
+ * Get content of the draft specified with an ID
+ * @param {String} draftId "r-xxxxxxxxxxxx"
+ * @returns {String} draftBody 
+ */
+// function getDraftContentById(draftId){
+//     return GmailApp.getDraft(draftId).getMessage().getPlainBody();
+// }
+
+function getDraftContentById(draftId) {
+    return GmailApp.getDraft(draftId).getMessage().getPlainBody();
+}
---
+++ high5-automail/test/test.js
@@ -0,0 +1,31 @@
+function testSendDraft(){
+    var draftId = "r-5318680169226253490";
+    var draft = getDraftById(draftId).getMessage();
+    var html = HtmlService.createHtmlOutput(draft.getPlainBody()).getContent();
+    GmailApp.sendEmail("hhoang.nov.13@gmail.com",
+        "drafts",
+        "abc",
+        {
+            htmlBody: html
+        });
+
+    var equal = HtmlService.createHtmlOutputFromFile("reminder").getContent();
+    GmailApp.sendEmail("hhoang.nov.13@gmail.com",
+        "file ",
+        "abc",
+        {
+            htmlBody: equal
+        });
+}
+
+function testOpenByUrl(){
+    openByUrl("https://docs.google.com/forms/d/1AfYPSl2l5QzYLe0L1wtfp9PX2tEctgiPBzw0a7_Vo98/edit");
+}
+
+function testCreateTrigger(){
+    var trig = 
+    ScriptApp
+    .newTrigger("testOpenByUrl")
+    .forForm(FormApp.openByUrl("https://docs.google.com/forms/d/1AfYPSl2l5QzYLe0L1wtfp9PX2tEctgiPBzw0a7_Vo98/edit"))
+    .onFormSubmit().create();
+}
